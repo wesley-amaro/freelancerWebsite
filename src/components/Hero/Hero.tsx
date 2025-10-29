f03e0d6f46a7e17434a3f66c596478510c93ed2a
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-10-29 11:03:54 -0300

    feat: Criado Hero.tsx, hero.css para animação de fundo e adicionado foto

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,21 @@
-function Hero() {
-  return <div>"Hero"</div>;
-}
+import React from "react";
+import heroPhoto from "../../assets/imagens/wes-sorriindo-party.jpg";
+import "./Hero.css"; //CSS separado para animação de fundo
+
+const Hero: React.FC = () => {
+  return (
+    <section className="hero">
+      <div className="hero-bg"></div>
+      {/* fundo animado */}
+      <div className="hero-content">
+        <img src={heroPhoto} alt="Wesley Amaro" className="hero-photo" />
+        <h1 className="hero-name">Wesley Amaro</h1>
+        <p className="hero-title font-bolder text-8xl">
+          Desenvolvedor Full-Stack com Abordagem Front-End
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
