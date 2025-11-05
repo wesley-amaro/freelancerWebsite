dd370e9e27e702f0f026a97becfac6b677a17fa8
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-11-05 17:01:19 -0300

    feat: Adicionado nova funcionalidade a pagina Hero(Home)

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
+import About from "./About/About";
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 };
