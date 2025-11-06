25159e5431444f8ee10a64019d577c5f00409253
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-11-06 15:57:55 -0300

    perf: Alteração no texto about, criação de array [] para melhor funcionalidade das tecnologias descritas e futuras

--- src/Pages/Home/About/About.tsx
+++ src/Pages/Home/About/About.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import fotoPrimaria from "../../../assets/imagens/wes-sorriindo-party.jpg";
 import fotoSecundaria from "../../../assets/imagens/wes-sorriindo-party.jpg";
 
+const tecnologias: string[] = [
+  "React",
+  "TypeScript",
+  "HTML",
+  "CSS",
+  "Java",
+  "Python",
+  "Tailwind",
+  "Material UI",
+  "Sass",
+  "SQL",
+  "GitHub",
+];
+
 const About: React.FC = () => {
   return (
     <section
@@ -27,7 +41,49 @@ const About: React.FC = () => {
         </div>
 
         {/* Coluna direita - Texto */}
-        <div></div>
+        <div className="flex flex-col justify-center space-y-6">
+          <h2 className="text-3xl sm:text-4xl font-bold tracking-tight text-neutral-100">
+            Sobre Mim
+          </h2>
+          <p className="text-base sm:text-lg leading-relaxed text-neutral-700">
+            👋🏽Olá! Sou{" "}
+            <strong>Wesley Amaro 👨🏽‍💻, Desenvolvedor Full Stack</strong> com foco
+            em <strong>Front-End</strong>. Trabalho com tecnologias{" "}
+            <strong>
+              como React, TypeScript, HTML, CSS, Sass, Tailwind e Material UI
+            </strong>{" "}
+            para criar interfaces modernas e bem estruturadas.
+          </p>
+
+          <p className="text-base sm:text-lg leading-relaxed text-neutral-700">
+            💫 Sou apaixonado por criar <strong>interfaces modernas,</strong>{" "}
+            funcionais e responsivas. Busco constantemente unir{" "}
+            <strong>
+              criatividade, performance e boas práticas de código 🔏
+            </strong>
+            , sempre com atenção aos detalhes visuais e à experiência do{" "}
+            <strong>usuário 🖥️.</strong>
+          </p>
+
+          <p className="text-base sm:text-lg leading-relaxed text-neutral-700">
+            🚀 Ajudo <strong>empresas e profissionais autônomos</strong> a
+            construírem presença digital com sites{" "}
+            <strong>modernos, rápidos e profissionais —</strong> que atraem mais
+            clientes e passam <strong>credibilidade</strong>.
+          </p>
+
+          {/* Tecnologias Dinamicas - Array [] */}
+          <div className="flex flex-wrap gap-3 pt-4">
+            {tecnologias.map((tech) => (
+              <span
+                key={tech}
+                className="px-4 py-1.5 rounded-full text-sm font-semibold bg-neutral-200 hover:bg-neutral-700 hover:text-white transition cursor-default"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
