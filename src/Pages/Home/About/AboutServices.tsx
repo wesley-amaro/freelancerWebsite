af03e65c03bfa65ab6fbd9a83279c83634c2789d
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-11-07 16:49:57 -0300

    feat: Criado nova pagina que será renderizada dentro do About.tsx

---
+++ src/Pages/Home/About/AboutServices.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutServices: React.FC = () => {
+  return (
+    <section className="services">
+      <div>
+        💻 O que eu faço Crio sites e landing pages personalizadas que ajudam
+        negócios a: 📈 Atrair mais clientes online 💬 Facilitar o contato com
+        clientes via WhatsApp 🧠 Transmitir mais confiança e profissionalismo ⚙️
+        Ter uma presença digital moderna e responsiva quero que minhas skills
+        tecnicas fiquem passando de um lado pro outro, como se fosse uma
+        esteira, andando para a direita (de lado){" "}
+      </div>
+    </section>
+  );
+};
+
+export default AboutServices;
