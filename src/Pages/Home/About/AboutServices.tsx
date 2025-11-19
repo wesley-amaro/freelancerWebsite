c9ba7911a9527b7f956645597b2a63b7d4c804d6
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-11-19 14:17:28 -0300

    style: mudado estilo de fundo da pagina e cor das letras

--- src/Pages/Home/About/AboutServices.tsx
+++ src/Pages/Home/About/AboutServices.tsx
@@ -4,19 +4,19 @@ const AboutServices: React.FC = () => {
   return (
     <section className="services">
       <div>
-        <h2 className="font-bold text-base sm:text-lg leading-relaxed text-neutral-700">
+        <h2 className="font-bold text-base sm:text-lg leading-relaxed text-cyan-400">
           💻 O que eu faço?
         </h2>
 
         <div>
-          <p className="text-base sm:text-lg leading-relaxed text-neutral-700">
+          <p className="text-base sm:text-lg leading-relaxed text-neutral-200">
             {" "}
             Crio sites e landing pages personalizadas que ajudam negócios a:{" "}
           </p>
 
           <br />
 
-          <p className="font-bold font-sans text-base sm:text-lg leading-relaxed text-neutral-800">
+          <p className="font-bold font-sans text-base sm:text-lg leading-relaxed text-neutral-200">
             📈 Atrair mais clientes online <br />
             💬 Facilitar o contato com clientes via WhatsApp <br />
             🧠 Transmitir mais confiança e profissionalismo <br />
