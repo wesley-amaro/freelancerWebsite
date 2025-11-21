74451011727ab374ec2e235c3ee7ad66180e381b
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-11-21 17:19:08 -0300

    feat/style: criado arquivo css, mudado fundo, proporção de tela estilo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import heroPhoto from "../../assets/imagens/wes-sorriindo-party.jpg";
 import "./Hero.css"; //CSS separado para animação de fundo
+import AboutCards from "../../Pages/Home/About/AboutCards";
 
 const Hero: React.FC = () => {
   return (
-    <div className="hero">
-      <video autoPlay loop muted playsInline className="video-bg">
-        <source src="/bg-mario-video.mp4" type="video/mp4" />
-      </video>
+    <>
+      <div className="hero hero-bg-animado">
+        {/* <video autoPlay loop muted playsInline className="video-bg">
+          <source src="/bg-mario-video.mp4" type="video/mp4" />
+        </video> */}
 
-      <div className="hero-content">
-        <img src={heroPhoto} alt="Wesley Amaro" className="hero-photo" />
-        <h1 className="hero-name">Wesley Amaro</h1>
-        <p className="hero-title font-bolder text-8xl">
-          Desenvolvedor Full-Stack com Abordagem Front-End
-        </p>
+        <div className="hero-content">
+          <img src={heroPhoto} alt="Wesley Amaro" className="hero-photo" />
+          <h1 className="hero-name">Wesley Amaro</h1>
+          <p className="hero-title font-bolder text-8xl">
+            Desenvolvedor Full-Stack com Abordagem Front-End
+          </p>
+        </div>
       </div>
-    </div>
+      <AboutCards />
+    </>
   );
 };
 
