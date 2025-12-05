f050548b61aebc2e862549d2264ae1895939fd95
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-12-05 16:50:42 -0300

    feat: criado Myservices e myservices.css para estilização de background

--- src/Pages/Home/MyServices/MyServices.tsx
+++ src/Pages/Home/MyServices/MyServices.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import "./my-projects.css";
 
 const MyServices: React.FC = () => {
   return (
-    <section className="services">
-      <div>"MyServices"</div>
-    </section>
+    <>
+      <section className="ms-animado relative w-screen min-h-screen flex items-center justify-center bg-[#131414] overflow-hidden py-20">
+        <div className="w-full">
+          <p className="text-white">
+            🧩 Meus serviços Serviço Descrição Valor médio Landing Page 1 página
+            de apresentação com links, WhatsApp e redes sociais. Entre em
+            contato comigo que te passo um orçamento Site Institucional 2–4
+            páginas (Home, Serviços, Contato, Sobre). Design personalizado.
+            Entre em contato comigo que te passo um orçamento Site com
+            Formulário/Integrações Envio de mensagens, orçamentos ou
+            agendamentos. Entre em contato comigo que te passo um orçamento 💡
+            Os valores podem variar conforme o tamanho e as funcionalidades do
+            projeto. Sempre envio um orçamento claro e personalizado antes de
+            começar. 🎨 Meus diferenciais ✅ Design moderno e responsivo ✅ Boas
+            práticas de Clean Code e SEO básico ✅ Suporte após a entrega ✅
+            Código organizado e fácil de manter ✅ Comunicação simples, sem
+            'tecnês'
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
