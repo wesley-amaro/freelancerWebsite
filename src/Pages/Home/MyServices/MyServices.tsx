f25766d3ef8db26f7d1e14aba9bcac94e6261f2c
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-12-09 15:00:54 -0300

    feat/perf: criado lógica para inserção de tabela, com arryas

--- src/Pages/Home/MyServices/MyServices.tsx
+++ src/Pages/Home/MyServices/MyServices.tsx
@@ -1,26 +1,47 @@
 import React from "react";
-import "./my-projects.css";
+import "./my-services.css";
+import MyServicesTable from "./MyServices-Table";
+
+const meusDados = [
+  {
+    id: 1,
+    service: "📝 Landing Page",
+    description:
+      "1 página de apresentação com links, WhatsApp e redes sociais.",
+    valor:
+      "Olá, tenho interesse na Landing Page. Poderia me enviar um orçamento?",
+  },
+  {
+    id: 2,
+    service: "💻 Site Institucional",
+    description:
+      "2–4 páginas (Home, Serviços, Contato, Sobre). Design personalizado. Entre em contato comigo que te passo um orçamento",
+    valor:
+      "Olá, tenho interesse no Site Institucional. Poderia me enviar um orçamento?",
+  },
+  {
+    id: 3,
+    service: "⚙️ Site com Formulário/Integrações",
+    description:
+      "Envio de mensagens, orçamentos ou agendamentos. Entre em contato comigo que te passo um orçamento",
+    valor:
+      "Olá, tenho interesse em um Site com Formulário/Integrações. Poderia me enviar um orçamento?",
+  },
+];
 
 const MyServices: React.FC = () => {
   return (
     <>
-      <section className="ms-animado relative w-screen min-h-screen flex items-center justify-center bg-[#131414] overflow-hidden py-20">
-        <div className="w-full">
-          <p className="text-white">
-            🧩 Meus serviços Serviço Descrição Valor médio Landing Page 1 página
-            de apresentação com links, WhatsApp e redes sociais. Entre em
-            contato comigo que te passo um orçamento Site Institucional 2–4
-            páginas (Home, Serviços, Contato, Sobre). Design personalizado.
-            Entre em contato comigo que te passo um orçamento Site com
-            Formulário/Integrações Envio de mensagens, orçamentos ou
-            agendamentos. Entre em contato comigo que te passo um orçamento 💡
-            Os valores podem variar conforme o tamanho e as funcionalidades do
-            projeto. Sempre envio um orçamento claro e personalizado antes de
-            começar. 🎨 Meus diferenciais ✅ Design moderno e responsivo ✅ Boas
-            práticas de Clean Code e SEO básico ✅ Suporte após a entrega ✅
-            Código organizado e fácil de manter ✅ Comunicação simples, sem
-            'tecnês'
+      <section className="w-full min-h-screen bg-white py-20 flex items-center justify-center ms-animado">
+        <div className="max-w-5xl w-full px-6 bg-amber-50 bg-opacity-400 rounded-lg shadow-lg">
+          <h1 className="text-4xl font-extrabold text-center mb-8 text-gray-800 py-5">
+            Tabela de Serviços
+          </h1>
+          <p className="text-center text-gray-800 mb-10 max-w-2xl mx-auto">
+            Aqui você encontra os principais serviços que ofereço, juntamente
+            com uma breve descrição e informações sobre o valor.
           </p>
+          <MyServicesTable dados={meusDados} />
         </div>
       </section>
     </>
